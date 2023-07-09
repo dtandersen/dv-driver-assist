63f6676e7d86ee5d89ed4460e788ab6512f1df9e
David Andersen <git@davidandersen.us>
2023-07-08 22:31:54 -0700

    delete pdb file

--- release.bat
+++ release.bat
@@ -1,6 +1,6 @@
 del /q build
 dotnet build
 dotnet test
-del "c:\Program Files (x86)\Steam\steamapps\common\Derail Valley\BepInEx\scripts\DriverAssist.dll
+del "c:\Program Files (x86)\Steam\steamapps\common\Derail Valley\BepInEx\scripts\DriverAssist.*
 copy /Y DriverAssist\bin\Debug\netframework4.8\DriverAssist.dll "c:\Program Files (x86)\Steam\steamapps\common\Derail Valley\BepInEx\plugins
 powershell -executionpolicy bypass .\package.ps1
