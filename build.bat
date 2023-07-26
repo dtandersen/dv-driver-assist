1db26c94fe01b48f6ce8216549c314f29fc01b51
David Andersen <git@davidandersen.us>
2023-07-25 19:58:18 -0700

    use xcopy to show filenames

--- build.bat
+++ build.bat
@@ -1,21 +1,26 @@
+@echo off
 dotnet build
 @REM del "c:\Program Files (x86)\Steam\steamapps\common\Derail Valley\BepInEx\plugins\DriverAssist.dll"
 @REM copy /Y DriverAssist\bin\Debug\netframework4.8\DriverAssist.dll "c:\Program Files (x86)\Steam\steamapps\common\Derail Valley\BepInEx\scripts
 
 
-del "c:\Program Files (x86)\Steam\steamapps\common\Derail Valley\BepInEx\scripts\DriverAssist.dll"
-copy /Y bin\Debug\DriverAssist.dll "c:\Program Files (x86)\Steam\steamapps\common\Derail Valley\BepInEx\scripts
+@REM del "c:\Program Files (x86)\Steam\steamapps\common\Derail Valley\BepInEx\scripts\DriverAssist.dll"
+@REM copy /Y bin\Debug\DriverAssist.dll "c:\Program Files (x86)\Steam\steamapps\common\Derail Valley\BepInEx\scripts
 
-del "c:\Program Files (x86)\Steam\steamapps\common\Derail Valley\BepInEx\scripts\DriverAssistBepInEx.dll"
-copy /Y bin\Debug\DriverAssistBepInEx.dll "c:\Program Files (x86)\Steam\steamapps\common\Derail Valley\BepInEx\scripts
+@REM del "c:\Program Files (x86)\Steam\steamapps\common\Derail Valley\BepInEx\scripts\DriverAssistBepInEx.dll"
+@REM copy /Y bin\Debug\DriverAssistBepInEx.dll "c:\Program Files (x86)\Steam\steamapps\common\Derail Valley\BepInEx\scripts
 
 
+@REM del "c:\Program Files (x86)\Steam\steamapps\common\Derail Valley\Mods\DriverAssist"
+@REM mkdir "c:\Program Files (x86)\Steam\steamapps\common\Derail Valley\Mods\DriverAssist"
+
+mkdir "c:\Program Files (x86)\Steam\steamapps\common\Derail Valley\Mods\DriverAssist"
 
 del "c:\Program Files (x86)\Steam\steamapps\common\Derail Valley\Mods\DriverAssist\Info.json"
-copy /Y Info.json "c:\Program Files (x86)\Steam\steamapps\common\Derail Valley\Mods\DriverAssist
+xcopy /Y /F Info.json "c:\Program Files (x86)\Steam\steamapps\common\Derail Valley\Mods\DriverAssist"
 
 del "c:\Program Files (x86)\Steam\steamapps\common\Derail Valley\Mods\DriverAssist\DriverAssist.dll"
-copy /Y bin\Debug\DriverAssist.dll "c:\Program Files (x86)\Steam\steamapps\common\Derail Valley\Mods\DriverAssist
+xcopy /Y /F bin\Debug\DriverAssist.dll "c:\Program Files (x86)\Steam\steamapps\common\Derail Valley\Mods\DriverAssist"
 
 @rem del "c:\Program Files (x86)\Steam\steamapps\common\Derail Valley\Mods\DriverAssist\DriverAssist.UMM.dll"
 @rem copy /Y bin\Debug\DriverAssist.UMM.dll "c:\Program Files (x86)\Steam\steamapps\common\Derail Valley\Mods\DriverAssist
